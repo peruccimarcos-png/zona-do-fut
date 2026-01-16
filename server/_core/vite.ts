336f64442112c95b434427023cbc2cdd66e26529
peruccimarcos-png <peruccimarcos@gmail.com>
2026-01-16 17:07:06 -0300

    Update vite.ts

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -6,7 +6,7 @@ import path from "path";
 import { createServer as createViteServer } from "vite";
 import viteConfig from "../../vite.config";
 
-export async function setupVite(app: Express, server: Server) {
+export async function setupVite(app: Express, server: Server ) {
   const serverOptions = {
     middlewareMode: true,
     hmr: { server },
@@ -48,10 +48,8 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath =
-    process.env.NODE_ENV === "development"
-      ? path.resolve(import.meta.dirname, "../..", "dist", "public")
-      : path.resolve(import.meta.dirname, "public");
+  const distPath = path.resolve(import.meta.dirname, "..", "public");
+  
   if (!fs.existsSync(distPath)) {
     console.error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`
